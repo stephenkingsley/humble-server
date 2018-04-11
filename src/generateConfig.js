86e61ad36a0b5c9171386d5c9369ee34fc34c112
stephen.kingsley <stephenkingsley@foxmail.com>
2018-04-11 18:06:17 +0800

    fix: config and middleware not exist

--- src/generateConfig.js
+++ src/generateConfig.js
@@ -5,40 +5,48 @@ const ENV = process.env.NODE_ENV;
 function getPath(folder) {
   const DEVMODE = process.env.DEV_MODE;
   let configFolderPath = folder;
+  let projectPath = __dirname.split('/node_modules')[0];
   if (DEVMODE === 'development') {
+    projectPath = path.dirname(__dirname);
     configFolderPath = `example/${folder}`;
   }
-  return path.join(path.dirname(__dirname), configFolderPath);
+  return path.join(projectPath, configFolderPath);
 }
 
 function generateConfig() {
   const DEVMODE = process.env.DEV_MODE;
   let configFolderPath = 'config';
+  let projectPath = __dirname.split('/node_modules')[0];
   if (DEVMODE === 'development') {
+    projectPath = path.dirname(__dirname);
     configFolderPath = 'example/config';
   }
-  const defaultFilePath = path.join(path.dirname(__dirname), configFolderPath, 'config.default.js');
-  const filePath = path.join(path.dirname(__dirname), configFolderPath, `config.${ENV}.js`);
+  const defaultFilePath = path.join(projectPath, configFolderPath, 'config.default.js');
+  const filePath = path.join(projectPath, configFolderPath, `config.${ENV}.js`);
   try {
-    // eslint-disable-line global-require
     const defaultConfig = require(defaultFilePath);
     const fileConfig = require(filePath);
     return Object.assign({}, defaultConfig, fileConfig);
   } catch (err) {
-    throw err;
+    console.warn(err);
   }
 }
 
 function generateMiddleware(config, router) {
+  if (!config) {
+    return [router.dispatch];
+  }
   const DEVMODE = process.env.DEV_MODE;
   let middlewareFolderPath = 'middleware';
+  let projectPath = __dirname.split('/node_modules')[0];
   if (DEVMODE === 'development') {
+    projectPath = path.dirname(__dirname);
     middlewareFolderPath = 'example/middleware';
   }
-  let middlewareConfig;
-  if (config.middleware && Array.isArray(config.middleware)) {
+  let middlewareConfig = [];
+  if (config.middleware && Array.isArray(config.middleware) && config.middleware.length > 0) {
     middlewareConfig = config.middleware.map(middleware => {
-      const filePath = path.join(path.dirname(__dirname), middlewareFolderPath, `${middleware}.js`);
+      const filePath = path.join(projectPath, middlewareFolderPath, `${middleware}.js`);
       const middlewareFunction = require(filePath);
       return middlewareFunction;
     });
